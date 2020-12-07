512cd99ddfcde2a3f268fb86e0927aac399ff9e6
root <root@ip-172-31-33-179.ap-south-1.compute.internal>
2020-12-07 14:44:08 +0000

    fifa

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld { 
    public static void main(String []args) {
       /* println() function to write Hello, World! */
-      System.out.println("We are live in india");     
+      System.out.println("Welcome to fifa");     
    }
 }
